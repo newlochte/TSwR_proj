0c47c3e7b5ca9a92dec6291778eb36cbd9f8bbba
Tymoteusz Tomczak <tymoteusztom@gmail.com>
2026-04-10 16:28:05 +0200

    Implement manipulator model without mass on the end. Updated parameters based on urdf file

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,16 +5,35 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.
+        self.r1 = 0.04
+        self.m1 = 3.
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
         self.m3 = 0.0
         self.r3 = 0.01
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+
+        self.d1 = self.l1 / 2
+        self.d2 = self.l2 / 2
+
+        self.alpha = (
+            self.m1 * self.d1**2
+            + self.I_1
+            + self.m2 * (self.l1**2 + self.d2**2)
+            + self.I_2
+        )
+
+        self.beta = (
+            self.m2 * self.l1 * self.d2
+            )
+
+        self.gamma = (
+            self.m2 * self.d2**2
+            + self.I_2
+            )
 
     def M(self, x):
         """
@@ -22,7 +41,13 @@ class ManiuplatorModel:
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        M_1_1 = self.alpha + 2 * self.beta * np.cos(q2)
+        M_1_2 = self.gamma + self.beta * np.cos(q2)
+        M_2_1 = self.gamma + self.beta * np.cos(q2)
+        M_2_2 = self.gamma
+
+        return np.array([[M_1_1, M_1_2], [M_2_1, M_2_2]])
 
     def C(self, x):
         """
@@ -30,4 +55,11 @@ class ManiuplatorModel:
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        C_1_1 = -self.beta * np.sin(q2) * q2_dot
+        C_1_2 = -self.beta * np.sin(q2) * (q1_dot + q2_dot)
+        C_2_1 =  self.beta * np.sin(q2) * q1_dot
+        C_2_2 = 0
+
+        return np.array([[C_1_1, C_1_2], [C_2_1, C_2_2]])
+
